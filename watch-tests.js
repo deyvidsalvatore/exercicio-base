d438f5eeb20be6874979409961ba8ddd3e69d379
DevSombrero <devsombrero@gmail.com>
2026-04-18 15:35:18 -0300

    fix: jest run

--- watch-tests.js
+++ watch-tests.js
@@ -1,21 +1,21 @@
-const { spawn } = require('child_process');
-const fs = require('fs');
+const { spawn } = require("child_process");
+const fs = require("fs");
 
-const triggerFile = '.trigger';
+const triggerFile = ".trigger";
 
 if (!fs.existsSync(triggerFile)) {
-  fs.writeFileSync(triggerFile, '');
+  fs.writeFileSync(triggerFile, "");
 }
 
-console.log('[TEST RUNNER] Aguardando o clique no botão RUN TESTS...');
+console.log("[TEST RUNNER] Aguardando o clique no botão RUN TESTS...");
 
 fs.watch(triggerFile, (eventType) => {
-  if (eventType === 'change') {
-    console.log('\n[TEST RUNNER] Disparando testes...\n');
+  if (eventType === "change") {
+    console.log("\n[TEST RUNNER] Disparando testes...\n");
 
-    spawn('npx', ['ng', 'test', '--watch=false', '--browsers=ChromeHeadless'], {
-      stdio: 'inherit',
-      shell: true
+    spawn("npx", ["ng", "test"], {
+      stdio: "inherit",
+      shell: true,
     });
   }
 });
