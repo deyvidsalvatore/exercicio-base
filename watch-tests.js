5c22b01b3733b96ead4d3af34e4e48ddb045af14
DevSombrero <devsombrero@gmail.com>
2026-04-18 19:08:20 -0300

    feat: rollback

--- watch-tests.js
+++ watch-tests.js
@@ -4,31 +4,19 @@ const fs = require("fs");
 const triggerFile = ".runner/.trigger";
 const resultFile = ".runner/.test-result";
 
-if (!fs.existsSync(".runner")) fs.mkdirSync(".runner");
-if (!fs.existsSync(triggerFile)) fs.writeFileSync(triggerFile, "");
-
-function limparLixoParaOSnapshot() {
-  const lixos = ["dist", "coverage", ".angular", ".vscode"];
-  lixos.forEach((pasta) => {
-    if (fs.existsSync(pasta)) {
-      try {
-        fs.rmSync(pasta, { recursive: true, force: true });
-        console.log(`Lixo removido: ${pasta}/`);
-      } catch (e) {}
-    }
-  });
+if (!fs.existsSync(".runner")) {
+  fs.mkdirSync(".runner");
 }
+
+if (!fs.existsSync(triggerFile)) fs.writeFileSync(triggerFile, "");
 
 console.log("[TEST RUNNER] Aguardando o clique no botão RUN TESTS...");
 
 fs.watch(triggerFile, (eventType) => {
   if (eventType === "change") {
     console.log("\n[TEST RUNNER] Disparando testes...\n");
+
     fs.writeFileSync(resultFile, "RUNNING");
-
-    limparLixoParaOSnapshot();
-
-    const faxinaEmTempoReal = setInterval(limparLixoParaOSnapshot, 1000);
 
     const testProcess = spawn("npx", ["ng", "test"], {
       stdio: "inherit",
@@ -36,12 +24,14 @@ fs.watch(triggerFile, (eventType) => {
     });
 
     testProcess.on("close", (code) => {
-      clearInterval(faxinaEmTempoReal);
-
-      limparLixoParaOSnapshot();
-
       const status = code === 0 ? "PASS" : "FAIL";
       fs.writeFileSync(resultFile, status);
+
+      if (status === "PASS") {
+        console.log("\nTODOS OS TESTES PASSARAM!");
+      } else {
+        console.log("\nALGUNS TESTES FALHARAM.");
+      }
     });
   }
 });
